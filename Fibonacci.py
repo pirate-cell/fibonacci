d7514c6bac7f36fd000a35e0840a2d58fced58f5
pirate-cell <84022705+pirate-cell@users.noreply.github.com>
2021-05-11 19:07:35 +0530

    Fibonacci.py
    Enter any number on screen

---
+++ Fibonacci.py
@@ -0,0 +1,14 @@
+# fibonacci
+Fibonacci series in python
+num=int(input("Enter any number:"))
+n1, n2= 0,1
+sum=0
+if num<=0:
+  print('please enter number greater than 0')
+  else:
+    for i in range (0, num):
+      print(sum,ends="")
+      n1 = n2
+      n2 = sum
+      sum = n1 + n2
+      
